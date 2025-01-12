0458a2f4a0d6e98e80fb1d47e9169d6246a9540b
asilvaolabi <asilva@olabi.org.br>
2025-01-11 21:13:41 -0300

    Refatorando e criando a autenticação

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,14 +1,55 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { StyledNavbar } from './styles'
+import React, { useEffect } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { auth } from "../../services/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import * as S from "./styles";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const [user] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user && window.location.pathname === "/") {
+      navigate("/dashboard");
+    }
+  }, [user, navigate]);
+
+  const signWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      await signInWithPopup(auth, provider);
+    } catch (error) {
+      console.error("Erro ao fazer login com Google", error);
+    }
+  };
+
+  const handleSignOut = async () => {
+    await signOut(auth);
+    navigate("/");
+  };
+
   return (
-    <StyledNavbar>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/criar-despesas">Minhas Despesas</NavLink>
-      <NavLink to="/dashboard">Dashboard</NavLink>
-    </StyledNavbar>
+    <S.StyledNavbar>
+      {!user && <NavLink to="/">Home</NavLink>}
+
+      {user && <NavLink to="/criar-despesas">Criar Despesas</NavLink>}
+
+      {user && <NavLink to="/dashboard">Dashboard</NavLink>}
+
+      {/* Exibe o botão de login ou a saudação com botão de sair */}
+      {user ? (
+        <S.ProfileContainer>
+          <S.UserInfo>
+            <S.UserImage src={user.photoURL} alt="Foto de perfil" />
+            <p>Olá, {user.displayName}!</p>
+          </S.UserInfo>
+          <S.Button onClick={handleSignOut}>Sair</S.Button>
+        </S.ProfileContainer>
+      ) : (
+        <S.Button onClick={signWithGoogle}>Login</S.Button>
+      )}
+    </S.StyledNavbar>
   );
 };
 
--- src/components/navbar/styles.ts
+++ src/components/navbar/styles.ts
@@ -1,7 +1,7 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const StyledNavbar = styled.nav`
-  background-color: #8B008B;
+  background-color: #8b008b;
   color: white;
   padding: 1rem;
   display: flex;
@@ -19,5 +19,57 @@ export const StyledNavbar = styled.nav`
   @media (max-width: 768px) {
     flex-direction: column;
     align-items: flex-start;
+  }
+`;
+
+export const ProfileContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #f9f9f9;
+  padding: 15px;
+  gap: 15px;
+  border-radius: 10px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+`;
+
+export const UserInfo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 15px;
+
+  p {
+    font-size: 18px;
+    font-weight: 500;
+    color: #333;
+    margin: 0;
+  }
+`;
+
+interface UserImageProps {
+  src?: string | null | undefined;
+}
+
+export const UserImage = styled.img<UserImageProps>`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  object-fit: cover;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+`;
+export const Button = styled.div`
+  display: inline-block;
+  background-color: #4caf50;
+  padding: 10px 20px;
+  border-radius: 5px;
+  color: white;
+  cursor: pointer;
+
+  label {
+    font-size: 16px;
+  }
+
+  &:hover {
+    background-color: #45a049;
   }
 `;
