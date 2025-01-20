7e4979016f58deddc4ce94b58d396cad1c9937e2
Nayara Nicacio <nayaranicacio@hotmail.com>
2025-01-20 18:10:25 -0300

    Teste-Cotacao-Dolar

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,118 @@
-// 
+// import { useEffect, useState } from "react";
+// import axios from "axios";
+// import * as S from "./styles";
+// import ChatGemini from "../components/chat-gemini/ChatGemini";
+// import { auth } from "../services/firebase";
+// import { useAuthState } from "react-firebase-hooks/auth";
+// import http from "../http";
+
+// type Despesa = {
+//   id: number;
+//   descricao: string;
+//   categoria: string;
+//   valor: number;
+//   tipo: string;
+//   data: string;
+//   user: string;
+// };
+
+// const Dashboard = () => {
+//   const [despesas, setDespesas] = useState([] as Despesa[]);
+//   const [user] = useAuthState(auth); // Hook do Firebase pa
+
+//   useEffect(() => {
+//     const fetchDespesas = async () => {
+//       try {
+//         const response = await http.get(`/despesas/${user?.uid}`);
+//         setDespesas(response.data);
+//       } catch (error) {
+//         console.error("Erro ao buscar despesas:", error);
+//       }
+//     };
+//     fetchDespesas();
+//   }, []);
+
+//   const calcularTotais = () => {
+//     if (despesas.length === 0) {
+//       return { entradas: 0, saidas: 0, saldo: 0 };
+//     }
+
+//     const entradas = despesas
+//       .filter((d) => d.tipo === "entrada" && d.valor)
+//       .reduce((acc, d) => acc + d.valor, 0);
+
+//     const saidas = despesas
+//       .filter((d) => d.tipo === "saída" && d.valor)
+//       .reduce((acc, d) => acc + d.valor, 0);
+
+//     return { entradas, saidas, saldo: entradas - saidas };
+//   };
+
+//   const { entradas, saidas, saldo } = calcularTotais();
+
+//   return (
+//     <S.TableContainer>
+//       <S.Title>Dashboard de Finanças</S.Title>
+//       {/* Totais de Entradas, Saídas e Saldo */}
+//       <S.CardsContainer>
+//         <S.Card bgColor="#FF8C00">
+//           <p>Entradas</p>
+//           <p>R$ {entradas.toFixed(2)}</p>
+//         </S.Card>
+//         <S.Card bgColor="#B22222">
+//           <p>Saídas</p>
+//           <p>R$ {saidas.toFixed(2)}</p>
+//         </S.Card>
+//         <S.Card bgColor="#006400">
+//           <p>Saldo</p>
+//           <p>R$ {saldo.toFixed(2)}</p>
+//         </S.Card>
+//         <S.Card bgColor="#ffc3333">
+//         <h1>Dólar Hoje</h1>
+//         <p id="dollar-rate">Carregando...</p>
+//         </S.Card> <S.Card bgColor="#FFC333">
+//           <h1>Dólar Hoje</h1>
+//           <p>{dollarRate ? `R$ ${dollarRate.toFixed(2)}` : "Carregando..."}</p>
+//         </S.Card>
+//       </S.CardsContainer>
+
+//       {/* Tabela com os dados das despesas */}
+//       <S.StyledTable>
+//         <thead>
+//           <tr>
+//             <th>Descrição</th>
+//             <th>Categoria</th>
+//             <th>Valor</th>
+//             <th>Tipo</th>
+//             <th>Data</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {despesas.map((despesa) => (
+//             <tr key={despesa.id}>
+//               <td>{despesa.descricao}</td>
+//               <td>{despesa.categoria}</td>
+//               <td>R$ {despesa.valor.toFixed(2)}</td>
+//               <td>{despesa.tipo}</td>
+//               <td>{despesa.data}</td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </S.StyledTable>
+//       <ChatGemini despesas={despesas} />
+//     </S.TableContainer>
+//   );
+// };
+
+// export default Dashboard;
+
 import { useEffect, useState } from "react";
-import * as S from "../styles";
-import { FetchDespesas } from "../../application/useCases/FetchDespesas";
-import { FetchDollarRate } from "../../application/useCases/FetchDollarRate";
-import { DespesasRepositoryImpl } from "../../infrastructure/repositories/DespesasRepositoryImpl";
-import { DollarRateServiceImpl } from "../../infrastructure/services/DollarRateServiceImpl";
-import { Despesa } from "../../domain/entities/Despesa";
+import axios from "axios";
+import * as S from "./styles";
+import ChatGemini from "../components/chat-gemini/ChatGemini";
+import { auth } from "../services/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import http from "../http";
 
 const Dashboard = () => {
   const [despesas, setDespesas] = useState<Despesa[]>([]);
