290417b0178207fb5d6924aa529cb65fcf35f2bd
Nayara Nicacio <nayaranicacio@hotmail.com>
2025-01-20 11:11:56 -0300

    Atualizado-Variaveis-Ambiente

--- src/services/firebase.tsx
+++ src/services/firebase.tsx
@@ -2,13 +2,35 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY_FIREBASE,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN_FIREBASE,
-  projectId: import.meta.env.VITE_PROJECT_ID_FIREBASE,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET_FIREBASE,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID_FIREBASE,
-  appId: import.meta.env.VITE_APP_ID_FIREBASE,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  appId: process.env.FIREBASE_APP_ID,
+
+  // apiKey: import.meta.env.VITE_API_KEY_FIREBASE,
+  // authDomain: import.meta.env.VITE_AUTH_DOMAIN_FIREBASE,
+  // projectId: import.meta.env.VITE_PROJECT_ID_FIREBASE,
+  // storageBucket: import.meta.env.VITE_STORAGE_BUCKET_FIREBASE,
+  // messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID_FIREBASE,
+  // appId: import.meta.env.VITE_APP_ID_FIREBASE,
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+
+/**
+ * Função para login com email e senha
+ * @param {string} email - O email do usuário
+ * @param {string} password - A senha do usuário
+ * @returns {Promise} - Retorna uma Promise com o usuário autenticado ou erro
+ */
+export const login = async (email, password) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    console.log("Login bem-sucedido:", userCredential.user);
+    return userCredential.user;
+  } catch (error) {
+    console.error("Erro ao realizar login:", error.code, error.message);
+    throw error; // Propaga o erro para quem chamar a função
+  }
+};
