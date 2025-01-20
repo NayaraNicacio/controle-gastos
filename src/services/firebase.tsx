cec4d8726dea5d279b55700b21aa75a9386e506c
Nayara Nicacio <nayaranicacio@hotmail.com>
2025-01-20 15:43:15 -0300

    Atualizado-Variaveis-Ambiente

--- src/services/firebase.tsx
+++ src/services/firebase.tsx
@@ -2,17 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  appId: process.env.FIREBASE_APP_ID,
-
-  // apiKey: import.meta.env.VITE_API_KEY_FIREBASE,
-  // authDomain: import.meta.env.VITE_AUTH_DOMAIN_FIREBASE,
-  // projectId: import.meta.env.VITE_PROJECT_ID_FIREBASE,
-  // storageBucket: import.meta.env.VITE_STORAGE_BUCKET_FIREBASE,
-  // messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID_FIREBASE,
-  // appId: import.meta.env.VITE_APP_ID_FIREBASE,
+  apiKey: import.meta.env.VITE_API_KEY_FIREBASE,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN_FIREBASE,
+  projectId: import.meta.env.VITE_PROJECT_ID_FIREBASE,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET_FIREBASE,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID_FIREBASE,
+  appId: import.meta.env.VITE_APP_ID_FIREBASE,
 };
 
 const app = initializeApp(firebaseConfig);
