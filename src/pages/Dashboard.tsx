db60935cc26a0d3e8075236c7bc2b4f23a398193
Nayara Nicacio <nayaranicacio@hotmail.com>
2025-01-22 23:41:24 -0300

    .

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 import * as S from "./styles";
 import ChatGemini from "../components/chat-gemini/ChatGemini";
 import { auth } from "../services/firebase";
